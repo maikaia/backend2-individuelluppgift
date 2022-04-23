a77b0a84106a3ddf087fcf7c0ba854dff84b3c84
Nils-Olov Olsson <nolle_olsson_@hotmail.com>
2022-04-23 12:15:13 +0200

    added create todo

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ const API_BASE = "http://localhost:3001"
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false)
+  const [newTodo, setNewTodo] = useState("")
   
   useEffect (() => {
     GetTodos ();
@@ -27,6 +29,21 @@ function App() {
       
             return todo;
       }))
+  }
+
+  const addTodo = async () => {
+    const data = await fetch(API_BASE + "/todo/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        text: newTodo
+      })
+    }).then(res => res.json())
+    setTodos([...todos, data])
+    setPopupActive(false)
+    setNewTodo("")
   }
 
   return (
@@ -53,8 +70,23 @@ function App() {
         </div>
        ))}
      </div>
-    </div>
+     <div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
 
+     {popupActive ? (
+       <div className="popup">
+         <div className="closePopup" onClick={() => setPopupActive(false)}>x</div>
+         <div className="content">
+           <h3>Add Task</h3>
+           <input 
+              type="text" 
+              className="add-todo-input" 
+              onChange={e => setNewTodo(e.target.value)} 
+              value={newTodo} />
+              <div className="button" onClick={addTodo}>Create task</div>
+         </div>
+       </div>
+     ) : ""}
+    </div>
   );
 }
 
